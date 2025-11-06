1f8e1b4612a9dcd6273bbb655582c2888dd48e41
slvn99 <slvn99@gmail.com>
2025-11-06 15:44:16 +0100

    another build fix

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -1,7 +1,5 @@
 import { ImageResponse } from 'next/og';
 
-export const runtime = 'edge';
-
 export async function GET() {
   const title = 'Second Horse Dating';
   const subtitle = 'Saddle up! Swipe your perfect pasture partner.';
